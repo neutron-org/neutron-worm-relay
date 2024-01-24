f71cf0cda8e803f97099099dda6dd5b00d540eb2
Julian Compagni Portis <julian@terrascope.io>
2024-01-24 08:52:02 -0700

    add relay app

--- src/app.ts
+++ src/app.ts
@@ -3,59 +3,111 @@ import {
   StandardRelayerApp,
   StandardRelayerContext,
 } from "@wormhole-foundation/relayer-engine";
-import { CHAIN_ID_SEPOLIA, TokenBridgePayload } from "@certusone/wormhole-sdk";
+import { CHAIN_ID_SEPOLIA, CHAIN_ID_ETH } from "@certusone/wormhole-sdk";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import {Tendermint37Client, TendermintClient} from "@cosmjs/tendermint-rpc";
+import { DirectSecp256k1Wallet } from "@cosmjs/proto-signing";
+import { GasPrice} from "@cosmjs/stargate";
+import { Secp256k1, keccak256 } from "@cosmjs/crypto";
+
+
+const hrp = "neutrond"
+const gasPrice = ".025ntrn"
+
+function getChainId() {
+    const chainName = process.env.EMITTER_CHAIN; 
+    switch(chainName){
+        case "sepolia":{
+            return CHAIN_ID_SEPOLIA;
+        }
+        case "ethereum": {
+            return CHAIN_ID_ETH;
+        }
+        default: {
+            throw new Error("invalid chain name");
+        }
+    }
+}
+
+export type Client = {
+    wasm: SigningCosmWasmClient;
+    // stargate: SigningStargateClient;
+    signer: string;
+    signer_addr: string;
+    signer_pubkey: string;
+};
+
+export async function getSigningClient(): Promise<Client> {
+    const signer = process.env.SIGNER;
+    const networkURL = process.env.NETOWRK_URL; 
+    const wallet = await DirectSecp256k1Wallet.fromKey(
+        Buffer.from(signer, "hex"), hrp);
+    const [account] = await wallet.getAccounts();
+    const clientBase: TendermintClient = await Tendermint37Client.connect(networkURL)
+
+    const wasm = await SigningCosmWasmClient.createWithSigner(
+    clientBase,
+    wallet,
+    {
+      gasPrice: GasPrice.fromString(gasPrice),
+    }
+  );
+
+  const pubkey = Secp256k1.uncompressPubkey(account.pubkey);
+  const ethaddr = keccak256(pubkey.slice(1)).slice(-20);
+
+  return {
+    wasm,
+    signer: account.address,
+    signer_addr: Buffer.from(ethaddr).toString("hex"),
+    signer_pubkey: Buffer.from(account.pubkey).toString("hex"),
+  };
+}
+
 
 (async function main() {
+    const emitter_address = process.env.EMITTER_ADDRESS;
+    const environment = process.env.ENVIRONMENT;
+    const ismAddr = process.env.ISM_ADDR;
+    const chain_id  = getChainId();
+    console.log("Starting relayer...\n " +
+        `environment: ${environment}` +
+        `chain_id: ${chain_id}` +
+        `emitter_address: ${emitter_address}`);
   // initialize relayer engine app, pass relevant config options
   const app = new StandardRelayerApp<StandardRelayerContext>(
-    Environment.TESTNET,
+      environment == "mainnet" ? Environment.MAINNET:  Environment.TESTNET,
     // other app specific config options can be set here for things
     // like retries, logger, or redis connection settings.
     {
-      name: `ExampleRelayer`,
+      name: `worm-neutron-relayer`,
     },
   );
+    const client = await getSigningClient();
+
+
 
   // add a filter with a callback that will be
   // invoked on finding a VAA that matches the filter
-  app.chain(CHAIN_ID_SEPOLIA).address(
-    "DZnkkTmCiFWfYTfT41X3Rd1kDgozqzxWaHqsw6W4x2oe", // emitter address on Solana
+  app.chain(chain_id).address(
+    emitter_address, 
     // callback function to invoke on new message
     async (ctx, next) => {
       ctx.logger.info(
         `Got a VAA with sequence: ${ctx.vaa?.sequence} from with txhash: ${ctx.sourceTxHash}`,
       );
 
-      const { payload } = ctx.tokenBridge;
+        const vaa = ctx.vaaBytes;
+        const {payload} = ctx.tokenBridge;
 
-      // only care about transfers
-      // TODO: do something more interesting than logging like:
-      // - redemption of VAA on target chain
-      // - tracking transfer amounts over time
-      switch (payload?.payloadType) {
-        case TokenBridgePayload.Transfer:
-          ctx.logger.info(
-            `Transfer processing for: \n` +
-              `\tToken: ${payload.tokenChain}:${payload.tokenAddress.toString(
-                "hex",
-              )}\n` +
-              `\tAmount: ${payload.amount}\n` +
-              `\tReceiver: ${payload.toChain}:${payload.to.toString("hex")}\n`,
-          );
-          break;
-        case TokenBridgePayload.TransferWithPayload:
-          ctx.logger.info(
-            `Transfer processing for: \n` +
-              `\tToken: ${payload.tokenChain}:${payload.tokenAddress.toString(
-                "hex",
-              )}\n` +
-              `\tAmount: ${payload.amount}\n` +
-              `\tSender ${payload.fromAddress?.toString("hex")}\n` +
-              `\tReceiver: ${payload.toChain}:${payload.to.toString("hex")}\n` +
-              `\tPayload: ${payload.tokenTransferPayload.toString("hex")}\n`,
-          );
-          break;
-      }
+        client.wasm.execute(client.signer,
+                       ismAddr,
+                       {SubmitMeta: {metadata: vaa,
+                                     message: payload
+                                    }
+                       }, "auto"
+                      )
+
 
       // invoke the next layer in the middleware pipeline
       next();
